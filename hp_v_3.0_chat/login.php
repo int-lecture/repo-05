b1b7b30187da42d07732478fa678da5103e57a7b
zentino <sn-z777@outlook.com>
2017-04-15 04:04:54 +0200

    Kommentare und header()

--- hp_v_3.0_chat/login.php
+++ hp_v_3.0_chat/login.php
@@ -1,19 +1,3 @@
-
-<?php
-		// Error Reporting komplett abschalten
-		error_reporting(0);
-		
-      	$chatpartner = $_GET["chatpartner"];
-		$username = $_GET["user"];
-		if($username==""){
-			echo "<h2>Geben Sie ihren Namen und den Namen </h2>"; 
-			echo "<h2>ihres Chatpartners ein</h2>";
-		}elseif($chatpartner!=""){
-			echo" <h2>Chatpartner</h2>";
-			echo" <h2><a href=Chi-Chat.php?user=".$username."&chatpartner=".$chatpartner.">".$chatpartner."</a></h2>";	
-		}
-?>
-
 <!doctype html>
 <html>
   <head>
@@ -23,23 +7,38 @@
   </head>
   
   <body>
-			<center><form method="get" action="">
-			<h4>Name</h4>
-			<input type="text" name="user" />
-			<br/>
-			<h4>Chatpartner</h4>
-			<input type="text" name="chatpartner"/>
-			<br/>
-			<input type="submit" value="Click!" />
-			</form></center>
+			<h2>Geben Sie ihren Namen und den Namen ihres Chatpartners ein </h2>
+  <?php
+		//http://stackoverflow.com/questions/1053424/how-do-i-get-php-errors-to-display
+		//Error Reporting komplett abschalten
+		error_reporting(0);
+		
+      		$chatpartner = $_GET["chatpartner"];
+		$username = $_GET["user"];
+		
+		if($username!="" and $chatpartner!=""){
+			//Automatisch weiterleiten zur Chatbox
+			header("Location:Chi-Chat.php?user=".$username."&chatpartner=".$chatpartner);
+		}
+?>		
+		<form method="GET" action=" ">
+		<center>
+		<h4>Name</h4>
+		<input type="text" name="user"/>
+		<br/>
+		<h4>Chatpartner</h4>
+		<input type="text" name="chatpartner"/>
+		<br/>
+		<input type="submit" value="Click!" />
+		</form></center>
 			
-			<div id="menu" class="menu">
-			<a href="index.html"		id="Hauptseite">Hauptseite</a>
-			<a href="Profil.html"		id="Firmenprofil">Firmenprofil</a>
-			<a href="Produkt.html" 		id="Produkt">Produkt</a>
-			<a href="Impressum.html"	id="Impressum">Impressum</a>
-			<a href="Newsletter.html"	id="Newsletter">Newsletter</a>
-			<a href="login.php"			id="login">Chat</a>
-			</div>
+		<div id="menu" class="menu">
+		<a href="index.html"		id="Hauptseite">Hauptseite</a>
+		<a href="Profil.html"		id="Firmenprofil">Firmenprofil</a>
+		<a href="Produkt.html" 		id="Produkt">Produkt</a>
+		<a href="Impressum.html"	id="Impressum">Impressum</a>
+		<a href="Newsletter.html"	id="Newsletter">Newsletter</a>
+		<a href="login.php"			id="login">Chat</a>
+		</div>
  </body> 
 </html>
