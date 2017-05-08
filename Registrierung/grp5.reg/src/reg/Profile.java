46d61fe53b86a26ef70bc6b98894c40e27785d5f
zentino <sn-z777@outlook.com>
2017-05-08 23:25:09 +0200

    Methode hinzugefügt

--- Registrierung/grp5.reg/src/reg/Profile.java
+++ Registrierung/grp5.reg/src/reg/Profile.java
@@ -11,7 +11,9 @@ public class Profile {
 	private String passwort;
 	/** User Email */
 	private String userEmail;
-
+	/** Token SessionID*/
+	private String token;
+	
 	private List<String> contact = new ArrayList<>();
 
 
@@ -20,8 +22,29 @@ public class Profile {
 		this.passwort = passwort;
 		this.userEmail = userEmail;
 	}
+	public Profile(String pseudonym,String passwort,String userEmail,String token){
+		this(pseudonym,passwort,userEmail);
+		this.token=token;
+	}
+	
 
 	public String getPseudonym(){
 		return this.pseudonym;
 	}
+	public String getUserEmail(){
+		return this.userEmail;
+	}
+	public String getToken(){
+		return this.token;
+	}
+	public JSONObject profileToJson() throws JSONException{
+		JSONObject j = new JSONObject();
+		String[] string = new String[contact.size()];
+		j.put("name", this.pseudonym);
+		j.put("email", this.userEmail);
+		j.put("contact", Arrays.toString(contact.toArray(new String[contact.size()])));
+
+		return j;
+	}
+
 }
