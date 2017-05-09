cab8816863c846d83ebc99e734981b3edbc16340
zentino <sn-z777@outlook.com>
2017-05-09 21:21:58 +0200

    port auf 5002 geändert + paket

--- Registrierung/grp5.reg/src/reg/GrizzlySrv.java
+++ Registrierung/grp5.reg/src/reg/GrizzlySrv.java
@@ -14,8 +14,8 @@ import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 public class GrizzlySrv {
 
 	public static void main(String[] args) throws IllegalArgumentException, IOException {
-		final String baseUri = "http://localhost:5001/";
-		final String paket = "srv";
+		final String baseUri = "http://localhost:5002/";
+		final String paket = "reg";
 
 		final Map<String, String> initParams = new HashMap<String, String>();
 
@@ -24,8 +24,8 @@ public class GrizzlySrv {
 		System.out.println("Starte grizzly...");
 		SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
 
-		System.out.printf("Grizzly läuft unter %s%n", baseUri);
-		System.out.println("[ENTER] drücken, um Grizzly zu beenden");
+		System.out.printf("Grizzly lÃ¤uft unter %s%n", baseUri);
+		System.out.println("[ENTER] drÃ¼cken, um Grizzly zu beenden");
 
 		System.in.read();
 		threadSelector.stopEndpoint();
