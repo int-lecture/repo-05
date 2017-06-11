95240c51ced65f022131e82ca93f7c502e141294
zentino <sn-z777@outlook.com>
2017-06-11 21:11:42 +0200

    grizzly

--- ChatServer/Chat-Server/src/server/GrizzlyServer.java
+++ ChatServer/Chat-Server/src/server/GrizzlyServer.java
@@ -14,22 +14,27 @@ import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 public class GrizzlyServer {
 
 	public static void main(String[] args) throws IllegalArgumentException, IOException {
-		final String baseUri = "http://localhost:5000/";
+		final String baseUri = "http://localhost:";
 		final String paket = "server";
 		final Map<String, String> initParams = new HashMap<String, String>();
 
 		initParams.put("com.sun.jersey.config.property.packages", paket);
 
 		System.out.println("Starte grizzly...");
-		SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
 
-		System.out.printf("Grizzly läuft unter %s%n", baseUri);
+		SelectorThread threadSelector1 = GrizzlyWebContainerFactory.create(baseUri+"4000/", initParams);
+		SelectorThread threadSelector2 = GrizzlyWebContainerFactory.create(baseUri+"4001/", initParams);
+		SelectorThread threadSelector3 = GrizzlyWebContainerFactory.create(baseUri+"4002/", initParams);
+
+		System.out.printf("Grizzlys laufen unter die Poeerts %s %s %s %n", baseUri+"4000/",baseUri+"4001/",baseUri+"4002/");
 		System.out.println("[ENTER] drücken, um Grizzly zu beenden");
 
 		System.in.read();
-		threadSelector.stopEndpoint();
+		threadSelector1.stopEndpoint();
+		threadSelector2.stopEndpoint();
+		threadSelector3.stopEndpoint();
 
-		System.out.println("Grizzly wurde beendet");
+		System.out.println("Grizzlys wurden getötet :(");
 		System.exit(0);
 
 	}
